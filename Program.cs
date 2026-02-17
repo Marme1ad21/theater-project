c326abbde99aaeef0fc6651b44902ac72a6c6864
Marme1ad21 <marme1ad21ten@gmail.com>
2026-02-17 21:45:00 +0300

    feat(Program): добавить точку входа с поддержкой UTF-8

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Theater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("=== ТЕАТР 'МЕЛОДИЯ' ===\n");
+
+            TheaterMenu menu = new TheaterMenu();
+            menu.ShowMainMenu();
+
+            Console.WriteLine("\nСпасибо за посещение театра! До новых встреч!");
+            Console.ReadKey();
+        }
+    }
+}
